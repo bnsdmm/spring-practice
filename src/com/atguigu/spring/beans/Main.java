5af589c2e3c9ec859fe642a9e3945358dea2a23b
wangzhen <506826727@qq.com>
2018-07-01 08:43:15 +0800

    2018-7-1

--- src/com/atguigu/spring/beans/Main.java
+++ src/com/atguigu/spring/beans/Main.java
@@ -11,9 +11,12 @@ public class Main {
 		 */
 		// 1. 创建 Spring 的IOC容器
 		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-
+		//ApplicationContext applicationContext1 = new ClassPathXmlApplicationContext("applicationContext.xml");
+		//System.out.println(applicationContext == applicationContext1);
 		// 2. 从容器中获取Bean
 		HelloWorld helloWorld = (HelloWorld) applicationContext.getBean("helloWorld");
+		//HelloWorld helloWorld1 = (HelloWorld) applicationContext.getBean("helloWorld");
+		//System.out.println(helloWorld==helloWorld1);
 		/*
 		 * HelloWorld helloWorld = applicationContext.getBean(HelloWorld.class);
 		 */
